b4fff942e1032c02b141cc7f5d6b4ad5a246e8ad
Tammy <Tammy21a2o@gmail.com>
2025-08-13 23:30:07 +0900

    add: Laravel project

---
+++ app/Http/Controllers/RemotePostController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use Exception;
+
+class RemotePostController extends Controller
+{
+    public function store(Request $request)
+    {
+        try {
+            $validated = $request->validate([
+                'title'     => 'required|string|max:255',
+                'author_id' => 'required|integer',
+                'content'   => 'nullable|string|max:1000',
+            ]);
+
+            $post = Post::create($validated);
+
+            return response()->json([
+                'message' => 'success post',
+                'data'    => $post
+            ]);
+        } catch (Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+}
